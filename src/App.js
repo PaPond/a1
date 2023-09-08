c58dc9ba79b9eb0be0f6583cf4234f7039bd1443
PaPond <pondrut1810@gmail.com>
2023-09-08 15:36:14 +0700

    add context-header and context-content
    add context-header and context-content

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ import {EventData} from './event-data'
 import RefsFunc from './refs-func'
 import RefsArray from './refs-array'
 import MassageBox from './state-func'
+import { userContext } from './context'
+import  Header2  from './context-header2'
+import  Content2  from './context-content2'
 
 export default function App(){
+  let [user,setUser] = React.useState()
   return (
   <> 
+  <userContext.Provider value={[user,setUser]}>
+    <Header2/><Content2/>
+  </userContext.Provider>
   <Banner/> 
   <p><center><Button/></center></p> 
   <Calculator/>
