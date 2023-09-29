e4c8244605e480778856f8b37a20bd431c615a7a
PaPond <pondrut1810@gmail.com>
2023-09-29 15:17:36 +0700

    Update Form
    Update Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Router3 from './router3'
+import FormCheck from './form-check'
+import FormBootstrap from './form-bootstrap'
 
 
 export default function App(){
   return (
   <> 
- <Router3/>
+ <FormCheck/>
   </>
   )
 }
