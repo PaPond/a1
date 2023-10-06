7bf1240b4468ed59c417d23fdafbf15e2896b993
PaPond <pondrut1810@gmail.com>
2023-10-06 15:15:31 +0700

    update form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Router3 from './router3'
 import FormCheck from './form-check'
-import FormBootstrap from './form-bootstrap'
+import FormValidation from './form-validation'
 
 
 export default function App(){
   return (
   <> 
- <FormCheck/>
+  <FormValidation/>
   </>
   )
 }
